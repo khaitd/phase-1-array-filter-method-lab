3e8f282b2807660b99435e92ad47058428396f45
Dmitriy <khaitdmitriy@gmail.com>
2022-03-28 14:00:57 -0400

    commit tree 77d748e14c9c234dd683989efd2858a7cbefcbe5

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+function findMatching(arr, name){
+   return arr.filter(element => element.toUpperCase() == name.toUpperCase() )
+}
+
+function fuzzyMatch(arr, name){
+    return arr.filter(element => element[0].toUpperCase() == name[0].toUpperCase() )
+}
+
+
+function matchName(driverArr, str){
+    return driverArr.filter(element => element.name == str )
+}
